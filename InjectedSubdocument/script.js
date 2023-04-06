6c1229f9b72a658999a64af0211442abd4949b2a
PiQuark6046 <piquark6046@proton.me>
2023-04-06 04:13:17 +0000

    Attach img elements into iframe

--- InjectedSubdocument/script.js
+++ InjectedSubdocument/script.js
@@ -7,12 +7,15 @@ Iframe2.src = 'about:blank'
 IframeBase.append(Iframe1, Iframe2)
 
 const IframeImageAG = Iframe1.contentWindow.document.createElement('img')
+const IframeImageuBO = Iframe2.contentWindow.document.createElement('img')
+Iframe1.contentWindow.document.append(IframeImageAG)
+Iframe2.contentWindow.document.append(IframeImageuBO)
+
 IframeImageAG.src = 'https://avatars.githubusercontent.com/u/8361145?s=200&v=4'
 IframeImageAG.id = 'IframeAdGuardLogo'
 IframeImageAG.width = '100'
 IframeImageAG.height = '100'
 
-const IframeImageuBO = Iframe2.contentWindow.document.createElement('img')
 IframeImageuBO.src = 'https://avatars.githubusercontent.com/u/18120975?s=200&v=4'
 IframeImageuBO.id = 'IframeuBOLogo'
 IframeImageuBO.width = '100'
