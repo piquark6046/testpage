b4bd4ecad46cd402408e76972d5b64c586f89af9
PiQuark6046 <piquark6046@proton.me>
2023-08-10 07:48:52 +0000

    Create XMLResponse-aagag

---
+++ XMLResponse-aagag/20230810/script.js
@@ -0,0 +1,17 @@
+"use strict";
+const XMLHttpRequestURL = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+const XMLHttpRequestMethod = 'GET';
+var XMLHttpRequestInstance = new XMLHttpRequest();
+XMLHttpRequestInstance.open(XMLHttpRequestMethod, XMLHttpRequestURL);
+XMLHttpRequestInstance.onloadend = function () {
+    let XHRResponseText = XMLHttpRequestInstance.responseText;
+    let XHRResponseURL = XMLHttpRequestInstance.responseURL;
+    let XHRResponseAdsbygoogle = (['google-adsense', 'googlesyndication'].some(function (item) { return XHRResponseText.includes(item); }));
+    document.querySelector('td#XHRResponseText').innerHTML = typeof XHRResponseText;
+    document.querySelector('td#XHRResponseURLEmpty').innerHTML = XHRResponseURL === '' ? 'true' : 'false';
+    document.querySelector('td#XHRResponseTextLength').innerHTML = XHRResponseText.length.toString();
+    document.querySelector('td#XHRResponseAdsbygoogle').innerHTML = XHRResponseAdsbygoogle.toString();
+    document.querySelector('td#XHRAdBlockCheck').innerHTML =
+        (!(typeof XHRResponseText === 'string' && XHRResponseURL && XHRResponseText)).toString();
+};
+XMLHttpRequestInstance.send();
